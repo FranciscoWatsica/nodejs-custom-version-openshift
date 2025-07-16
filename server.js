b1612e84eda8d57aa3cebbc961b9a10f23f1bd78
ubuntu <ubuntu@localhost.localdomain>
2025-07-16 18:27:35 +0000

    docs: add comments to server

--- server.js
+++ server.js
@@ -177,4 +177,4 @@ var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
 
-                                                                                              
+                                                                                               
